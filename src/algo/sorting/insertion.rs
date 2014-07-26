825a86cc0c03f3669e516e49c8b88d8b46856230
Nathan Typanski <ntypanski@gmail.com>
2014-07-26 05:29:31 -0400

    sorting: divide into submodules

--- src/algo/sorting/insertion.rs
+++ src/algo/sorting/insertion.rs
@@ -4,7 +4,7 @@ use std::vec::Vec;
 
 /// Implementation of insertion sort.
 /// CLRS pg 18. `O(n^2)`.
-fn insertion_sort<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+pub fn insertion_sort<T: Ord>(mut a: Vec<T>) -> Vec<T> {
     // Break it apart so we can do tricky memory things.
     let len = a.len();
     let cap = a.capacity();
@@ -22,31 +22,6 @@ fn insertion_sort<T: Ord>(mut a: Vec<T>) -> Vec<T> {
                 i = i - 1;
             }
             ptr::write(p.offset(i+1), key);
-        }
-        Vec::from_raw_parts(len, cap, p)
-    }
-}
-
-/// Implementation of selection sort.
-/// CLRS pg 29. `O(n^2)`.
-fn selection_sort<T: Ord>(mut a: Vec<T>) -> Vec<T> {
-    let len = a.len();
-    let cap = a.capacity();
-    let p = a.as_mut_ptr();
-    unsafe {
-        mem::forget(a);
-        for j in range(0, (len - 1) as int) {
-            let mut smallest = j;
-            for i in range(j + 1, (len) as int) {
-                let elem = ptr::read(p.offset(i) as *const T);
-                let smallest_elem = ptr::read(p.offset(smallest) as *const T);
-                if elem < smallest_elem {
-                    smallest = i;
-                }
-            }
-            if smallest != j {
-                ptr::swap(p.offset(smallest), p.offset(j));
-            }
         }
         Vec::from_raw_parts(len, cap, p)
     }
@@ -82,35 +57,6 @@ mod tests {
     fn test_insertion_sort_empty() {
         let mut sorted = vec!();
         sorted = sorting::insertion_sort::<i32>(sorted);
-        assert_eq!(sorted, vec!());
-    }
-
-    #[test]
-    fn test_selection_sort_example() {
-        // Example from CLRS pg 18 (originally for insertion sort).
-        let mut sorted = vec!(5i32, 2, 4, 6, 1, 3);
-        sorted = sorting::selection_sort(sorted);
-        assert_eq!(sorted, vec!(1i32, 2, 3, 4, 5, 6));
-    }
-
-    #[test]
-    fn test_selection_sort_duplicates() {
-        let mut sorted = vec!(5i32, 5, 4);
-        sorted = sorting::selection_sort(sorted);
-        assert_eq!(sorted, vec!(4, 5, 5));
-    }
-
-    #[test]
-    fn test_selection_sort_single_element() {
-        let mut sorted = vec!(0i32);
-        sorted = sorting::selection_sort(sorted);
-        assert_eq!(sorted, vec!(0));
-    }
-
-    #[test]
-    fn test_selection_sort_empty() {
-        let mut sorted = vec!();
-        sorted = sorting::selection_sort::<i32>(sorted);
         assert_eq!(sorted, vec!());
     }
 }
---
+++ src/algo/sorting/mod.rs
@@ -0,0 +1,6 @@
+
+pub use self::insertion::insertion_sort;
+pub use self::selection::selection_sort;
+
+mod insertion;
+mod selection;
