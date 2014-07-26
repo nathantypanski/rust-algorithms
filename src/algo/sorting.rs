fedc9ad1a026b04f11cd5b03f22189a9a575cb16
Nathan Typanski <ntypanski@gmail.com>
2014-07-26 04:53:39 -0400

    insertion sort: generic version

--- src/algo/sorting.rs
+++ src/algo/sorting.rs
@@ -1,24 +1,24 @@
 use std::ptr;
 use std::mem;
 use std::vec::Vec;
-use std;
 
 /// Implementation of insertion sort.
-/// CLRS pg 18.
-///
-///     O(n^2)
-///
-fn insertion_sort(mut a: Vec<i32>) -> Vec<i32> {
+/// CLRS pg 18. `O(n^2)`.
+fn insertion_sort<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+    // Break it apart so we can do tricky memory things.
     let len = a.len();
     let cap = a.capacity();
     let p = a.as_mut_ptr();
     unsafe {
+        // Stop the memory manager from calling the destructor.
         mem::forget(a);
         for j in range(1, len as int) {
-            let key = *p.offset(j);
+            let key_offset = p.offset(j) as *const T;
+            let key = ptr::read(key_offset);
             let mut i = j - 1;
             while i > -1 && *p.offset(i) > key {
-                ptr::write(p.offset(i + 1), *p.offset(i));
+                let other_offset = p.offset(i) as *const T;
+                ptr::write(p.offset(i + 1), ptr::read(other_offset));
                 i = i - 1;
             }
             ptr::write(p.offset(i+1), key);
@@ -34,21 +34,21 @@ mod tests {
     #[test]
     fn test_insertion_sort_example() {
         // Example from CLRS pg 18.
-        let mut sorted = vec!(5, 2, 4, 6, 1, 3);
+        let mut sorted = vec!(5i32, 2, 4, 6, 1, 3);
         sorted = sorting::insertion_sort(sorted);
         assert_eq!(sorted, vec!(1i32, 2, 3, 4, 5, 6));
     }
 
     #[test]
     fn test_insertion_sort_duplicates() {
-        let mut sorted = vec!(5, 5, 4);
+        let mut sorted = vec!(5i32, 5, 4);
         sorted = sorting::insertion_sort(sorted);
         assert_eq!(sorted, vec!(4, 5, 5));
     }
 
     #[test]
     fn test_insertion_sort_single_element() {
-        let mut sorted = vec!(0);
+        let mut sorted = vec!(0i32);
         sorted = sorting::insertion_sort(sorted);
         assert_eq!(sorted, vec!(0));
     }
@@ -56,7 +56,7 @@ mod tests {
     #[test]
     fn test_insertion_sort_empty() {
         let mut sorted = vec!();
-        sorted = sorting::insertion_sort(sorted);
+        sorted = sorting::insertion_sort::<i32>(sorted);
         assert_eq!(sorted, vec!());
     }
 }
