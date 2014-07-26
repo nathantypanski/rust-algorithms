95566c02e594cde4a6731edbd058ad41d0782209
Nathan Typanski <ntypanski@gmail.com>
2014-07-26 05:07:16 -0400

    selection sort: implement

--- src/algo/sorting.rs
+++ src/algo/sorting.rs
@@ -27,6 +27,31 @@ fn insertion_sort<T: Ord>(mut a: Vec<T>) -> Vec<T> {
     }
 }
 
+/// Implementation of selection sort.
+/// CLRS pg 29. `O(n^2)`.
+fn selection_sort<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+    let len = a.len();
+    let cap = a.capacity();
+    let p = a.as_mut_ptr();
+    unsafe {
+        mem::forget(a);
+        for j in range(0, (len - 1) as int) {
+            let mut smallest = j;
+            for i in range(j + 1, (len) as int) {
+                let elem = ptr::read(p.offset(i) as *const T);
+                let smallest_elem = ptr::read(p.offset(smallest) as *const T);
+                if elem < smallest_elem {
+                    smallest = i;
+                }
+            }
+            if smallest != j {
+                ptr::swap(p.offset(smallest), p.offset(j));
+            }
+        }
+        Vec::from_raw_parts(len, cap, p)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use sorting;
@@ -57,6 +82,35 @@ mod tests {
     fn test_insertion_sort_empty() {
         let mut sorted = vec!();
         sorted = sorting::insertion_sort::<i32>(sorted);
+        assert_eq!(sorted, vec!());
+    }
+
+    #[test]
+    fn test_selection_sort_example() {
+        // Example from CLRS pg 18 (originally for insertion sort).
+        let mut sorted = vec!(5i32, 2, 4, 6, 1, 3);
+        sorted = sorting::selection_sort(sorted);
+        assert_eq!(sorted, vec!(1i32, 2, 3, 4, 5, 6));
+    }
+
+    #[test]
+    fn test_selection_sort_duplicates() {
+        let mut sorted = vec!(5i32, 5, 4);
+        sorted = sorting::selection_sort(sorted);
+        assert_eq!(sorted, vec!(4, 5, 5));
+    }
+
+    #[test]
+    fn test_selection_sort_single_element() {
+        let mut sorted = vec!(0i32);
+        sorted = sorting::selection_sort(sorted);
+        assert_eq!(sorted, vec!(0));
+    }
+
+    #[test]
+    fn test_selection_sort_empty() {
+        let mut sorted = vec!();
+        sorted = sorting::selection_sort::<i32>(sorted);
         assert_eq!(sorted, vec!());
     }
 }
